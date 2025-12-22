72760413b0fb631fdee32bce0a88aae5c0fe4d0b
Serhat <mserhatbalik@gmail.com>
2025-12-22 22:21:31 +0300

    Refactored keyup and keydown events

--- ship.py
+++ ship.py
@@ -30,8 +30,9 @@ class Ship:
         # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0:
+        if self.moving_left and self.rect.left > self.screen_rect.left:
             self.x -= self.settings.ship_speed
+
 
         self.rect.x = self.x
 
