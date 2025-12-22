a22472e0a4f67ee36806c9e83acc13cd7138d340
Serhat <mserhatbalik@gmail.com>
2025-12-22 21:37:22 +0300

    First commit as a seperate project

---
+++ alien_invasion.py
@@ -0,0 +1,69 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        # Sets the display window to 1200x800 pixel
+        # Also Returns a "Surface Object" with 1200x800 dimensions and assigns it to the "screen" property.
+        # We will work with this property later.
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        # We pass the (self) as the argument to Ship class, where we use all the properties of AlienInvasion class with ai_game argument in Ship class.
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            # Set the framerate to 60. It effectively sets a "sleep" time to compensate for fast computers. It waits if the code above in the loop executes fast.
+            self.clock.tick(60)
+
+    # "helper" methods are used inside classes to simplify code, they are not meant to be called outside the class.
+    # helper methods are named with "_" prefix
+    def _check_events(self):
+        # Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
+
+# TODO
+# Allowing Continuous Movement
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all settings for the Alien Invasion game."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        self.screen_width = 1200
+        self.screen_height = 800
+        # self.bg_color = (230, 230, 230)
+
+        # EXERCISE 12-1
+        self.bg_color = (115, 178, 255)
