34f53633755a1a5f72834d4994d4b528becc677f
Serhat <mserhatbalik@gmail.com>
2025-12-22 23:56:29 +0300

    Created basic bullet class

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,13 @@ class AlienInvasion:
         # Also Returns a "Surface Object" with 1200x800 dimensions and assigns it to the "screen" property.
         # We will work with this property later.
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        ### FULLSCREEN SETTINGS ###
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        ###########################
+
         pygame.display.set_caption("Alien Invasion")
 
         # We pass the (self) as the argument to Ship class, where we use all the properties of AlienInvasion class with ai_game argument in Ship class.
--- settings.py
+++ settings.py
@@ -8,9 +8,13 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 5
+        self.ship_speed = 9.5
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
 
         # EXERCISE 12-1
         # self.bg_color = (115, 178, 255)
-
-
