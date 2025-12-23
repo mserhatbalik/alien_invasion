882f8d26787458bd647fd8c2fa2b1a59d668a7cc
Serhat <mserhatbalik@gmail.com>
2025-12-23 05:16:35 +0300

    Created basic alien class

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
-
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -30,6 +30,9 @@ class AlienInvasion:
         # We pass the (self) as the argument to Ship class, where we use all the properties of AlienInvasion class with ai_game argument in Ship class.
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -91,9 +94,16 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        # Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
 if __name__ == '__main__':
