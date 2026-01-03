f63b0d81a9893822a5ccf07e4a1db733a583a64a
Serhat <mserhatbalik@gmail.com>
2026-01-03 16:12:42 +0300

    Placed the play button into start screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -41,8 +42,10 @@ class AlienInvasion:
         self._create_fleet()
 
         # Start Alien Invasion in an active state
-        self.game_active = True
+        self.game_active = False
 
+        # Create the Play button
+        self.play_button = Button(self, "Play")
 
 
     def run_game(self):
@@ -133,6 +136,10 @@ class AlienInvasion:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive
+        if not self.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
