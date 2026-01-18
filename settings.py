412d6e1fd2f2d5401e64197511b8190353d08412
Serhat <mserhatbalik@gmail.com>
2026-01-18 18:33:47 +0300

    Added remaining ship count as sprites to the dashboard.

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_limit = 1
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_width = 3
